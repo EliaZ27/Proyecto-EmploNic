65d271b5bdef8f0e4417e93cd44814bcd209b46e
Mariela Cecilia Rodríguez Hernández <Mariela Cecilia Rodrguez Hernndez>
2016-11-13 00:56:34 -0600

    Cuarto Comit

--- app/src/main/java/com/emplonic/brothersapp/emplonic/MainActivity.java
+++ app/src/main/java/com/emplonic/brothersapp/emplonic/MainActivity.java
@@ -5,25 +5,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity{
 
-
     Button mbtn;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mbtn = (Button) findViewById(R.id.btnLogin);
+        btn = (Button) findViewById(R.id.btnRegistrate);
         mbtn.setOnClickListener(new  View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Main_Drawer_Emplofind.class);
+                    Intent intent = new Intent(MainActivity.this, Main_Drawer_Emplofind.class);
+                    startActivity(intent);
+            }
+        });
+
+        btn.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main_Register.class);
                 startActivity(intent);
             }
-
         });
+
+
     }
 
 
